d28dd4aab7607c37c39fa3d836cb4f16d3723685
Lamb555-Ke <kevinwashere555@gmail.com>
2025-02-06 10:54:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const messages = [
-    "Are you sure?",
+    "Are you sure? :(",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
+    "Bubba pleaseeeee",
+    "Just think about it for a sec...",
+    "If you say no one more time, ill get really sad :(",
+    "I'll be super sad.....",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, say yes please! (this is the last one so say yes) ❤️"
+    "Ugh ok fine hmph"
 ];
 
 let messageIndex = 0;
